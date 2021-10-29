a2e1ff9af3d187208e16f03df8a37411d0877d2b
Guille Polito <guillermopolito@gmail.com>
2021-10-29 12:01:32 +0200

    Rendering sprites in SDL

--- src/main.c
+++ src/main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include "SDL.h"
+#include "chip8/chip8.h"
 
-int main(int argc, char **argv) {
+#define PIXEL_SIZE 10
 
+int r,g,b = 0;
+int x , y = 10;
+SDL_Window * window;
+
+SDL_Renderer * initialize_graphics(){
 	SDL_Init(SDL_INIT_VIDEO);
 
-	SDL_Window *window = SDL_CreateWindow(
+	window = SDL_CreateWindow(
 			"SDL2Test",
 			SDL_WINDOWPOS_UNDEFINED,
 			SDL_WINDOWPOS_UNDEFINED,
-			640,
-			480,
+			64 * PIXEL_SIZE,
+			32 * PIXEL_SIZE,
 			0
 	);
 
@@ -18,39 +24,57 @@ int main(int argc, char **argv) {
 	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE);
 	SDL_RenderClear(renderer);
 	SDL_RenderPresent(renderer);
+	return renderer;
+}
 
-	int r,g,b = 0;
-	int x , y =10;
+void manage_events(){
 	SDL_Event event;
-	while (1) {
-		SDL_PollEvent(&event);
-		if (event.type == SDL_QUIT){
-			SDL_DestroyWindow(window);
-			SDL_Quit();
-			return 0;
+	SDL_PollEvent(&event);
+	if (event.type == SDL_QUIT){
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		exit(0);
+	}
+}
+
+void draw(SDL_Renderer *renderer){
+	//Clear screen
+	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+	SDL_RenderClear(renderer);
+
+	SDL_SetRenderDrawColor( renderer, 0x00, 0x00, 0xFF, 0xFF );
+
+	// The graphical Memory is 64 cells wide and 32 high
+	for(int x = 0; x < 64; x++){
+		for(int y = 0; y < 32; y++){
+			int pixel = chip8_graphical_memory[x][y];
+			//If the pixel is on, we should draw it
+			if (pixel > 0){
+				SDL_Rect fillRect = { x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE };
+				SDL_RenderFillRect( renderer, &fillRect );
+			}
 		}
+	}
 
-		r += 50;
-		g += 70;
-		b += 20;
+	SDL_RenderPresent( renderer );
+}
 
-		x += 1;
-		y += 2;
+int main(int argc, char **argv) {
 
-		x %=200;
-		y %=200;
+	SDL_Renderer *renderer = initialize_graphics();
+	chip8_init();
+	chip8_set_register_value(0, 0);
+	chip8_index = CHIP8_PROGRAM_START + 2;
 
-		SDL_Rect fillRect = { x, y, x + 10, y + 10 };
+	// sprite v0 v0 5
+	uint8_t bytes[7] = { 0xD0, 0x05, 0x18, 0x24, 0x3C, 0x24, 0x24 };
+	chip8_load(bytes, 7);
 
-		//Clear screen
-		SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF );
-		SDL_RenderClear(renderer);
+	chip8_step();
 
-		//Redraw
-		SDL_SetRenderDrawColor( renderer, r, g, b, 0xFF );
-		SDL_RenderFillRect( renderer, &fillRect );
-		SDL_RenderPresent( renderer );
-
+	while (1) {
+		manage_events();
+		draw(renderer);
 	}
 
 	return 0;
