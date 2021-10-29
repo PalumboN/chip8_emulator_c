0f4cfffab74a331b50d4382c39b3abfa0ebe1124
Guille Polito <guillermopolito@gmail.com>
2021-10-29 10:21:27 +0200

    Refactoring to simplify dispatching

--- src/chip8.c
+++ src/chip8.c
@@ -65,6 +65,41 @@ void chip8_set_register_value(uint8_t register_id, uint8_t value) {
 	chip8_registers[register_id] = value;
 }
 
+void chip8_doJump(uint16_t instruction){
+	chip8_instruction_pointer = instruction & 0x0FFF;
+}
+
+void chip8_doSprite(uint16_t instruction){
+	// sprite VX VY N
+	uint8_t x_register_id = (instruction & 0x0F00) >> 8;
+	uint8_t y_register_id = (instruction & 0x00F0) >> 4;
+	uint8_t sprite_height = instruction & 0x000F;
+	uint8_t x_value = chip8_get_register_value_unsafe(x_register_id);
+	uint8_t y_value = chip8_get_register_value_unsafe(y_register_id);
+
+	for(int row = 0; row < sprite_height; row++){
+		uint8_t sprite_row = memory[chip8_index + row];
+		for(int bit_position = 0; bit_position < 8; bit_position++){
+			uint8_t pixel = read_bit(sprite_row, bit_position);
+			chip8_graphical_memory[x_value + bit_position][y_value + row] = pixel;
+		}
+	}
+}
+
+void chip8_doAdd(uint16_t instruction){
+	// VX += VY
+	uint8_t left_register_id = (instruction & 0x0F00) >> 8;
+	uint8_t right_register_id = (instruction & 0x00F0) >> 4;
+	uint8_t left_value = chip8_get_register_value_unsafe(left_register_id);
+	uint8_t right_value = chip8_get_register_value_unsafe(right_register_id);
+
+	uint16_t result = left_value + right_value;
+	chip8_overflow_register = (result & 0xFF00) > 0 ? 1 : 0;
+
+	chip8_set_register_value(left_register_id, result & 0xFF);
+	chip8_instruction_pointer += 2;
+}
+
 void chip8_step(){
 	uint8_t higher = memory[chip8_instruction_pointer];
 	uint8_t lower = memory[chip8_instruction_pointer + 1];
@@ -72,34 +107,11 @@ void chip8_step(){
 
 	//If this is a JUMP
 	if ((currentInstruction & 0xF000) == 0x1000){
-		chip8_instruction_pointer = currentInstruction & 0x0FFF;
+		chip8_doJump(currentInstruction);
 	} else if ((currentInstruction & 0xF000) == 0xD000) {
-		// sprite VX VY N
-		uint8_t x_register_id = (currentInstruction & 0x0F00) >> 8;
-		uint8_t y_register_id = (currentInstruction & 0x00F0) >> 4;
-		uint8_t sprite_height = currentInstruction & 0x000F;
-		uint8_t x_value = chip8_get_register_value_unsafe(x_register_id);
-		uint8_t y_value = chip8_get_register_value_unsafe(y_register_id);
-
-		for(int row = 0; row < sprite_height; row++){
-			uint8_t sprite_row = memory[chip8_index + row];
-			for(int bit_position = 0; bit_position < 8; bit_position++){
-				uint8_t pixel = read_bit(sprite_row, bit_position);
-				chip8_graphical_memory[x_value + bit_position][y_value + row] = pixel;
-			}
-		}
+		chip8_doSprite(currentInstruction);
 	} else if ((currentInstruction & 0xF000) == 0x8000) {
-		// VX += VY
-		uint8_t left_register_id = (currentInstruction & 0x0F00) >> 8;
-		uint8_t right_register_id = (currentInstruction & 0x00F0) >> 4;
-		uint8_t left_value = chip8_get_register_value_unsafe(left_register_id);
-		uint8_t right_value = chip8_get_register_value_unsafe(right_register_id);
-
-		uint16_t result = left_value + right_value;
-		chip8_overflow_register = (result & 0xFF00) > 0 ? 1 : 0;
-
-		chip8_set_register_value(left_register_id, result & 0xFF);
-		chip8_instruction_pointer += 2;
+		chip8_doAdd(currentInstruction);
 	} else{
 		chip8_instruction_pointer += 2;
 	}
