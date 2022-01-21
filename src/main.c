c0b278a4bef038b0f1d0d51f8f40e05017d5814f
palumbon <nahuel.palumbo@gmail.com>
2022-01-21 11:45:21 +0100

    Building with CMake

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+#include "chip8/assembler.h"
 
 int main(int argc, char **argv) {
+	// Testing dependency
+	chip8_program_ptr program;
+	program = chip8_assembler_init();
+	chip8_jump(program, 0x789);
+	// Testing dependency
+
 	printf("Hello world");
 	return 0;
 }
